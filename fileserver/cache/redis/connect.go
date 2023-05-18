d2f70a4f300cda84f9b15047d91c9b22c802e27e
Lenny Mo <lennymo.work@gmail.com>
2023-05-18 13:18:34 +0800

    fix bug which would cause user log in error

--- fileserver/cache/redis/connect.go
+++ fileserver/cache/redis/connect.go
@@ -1,2 +1,26 @@
 // 管理与redis的连接池
 package redis
+
+import "github.com/go-redis/redis"
+
+var (
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6380", // connect to localhost
+		Password: "",               // default no password
+		DB:       0,                // use default DB
+		PoolSize: 100,              // 连接池大小, 默认是10,
+	})
+)	
+
+func NewRedisClient() *redis.Client {  
+	return redisClient
+}
+
+func CloseRedisClient() {
+	redisClient.Close()
+}
+
+// 测试redis连接是否正常
+func Ping() error {
+	return redisClient.Ping().Err()
+}
